88ba652a2fe3d18df2c314dacb2db4c4650e5c0b
Kirill Temnenkov <kirill@temnenkov.com>
2014-02-24 12:03:29 +0400

    screenshot

--- src/main/java/srobot/App.java
+++ src/main/java/srobot/App.java
@@ -1,13 +1,24 @@
 package srobot;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws AWTException, IOException {
+        Robot robot = new Robot();
+        Rectangle area = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        BufferedImage image = robot.createScreenCapture(area);
+        ImageIO.write(image, "BMP", new File("c:/temp/screenShot.bmp"));
+
+
+
     }
 }
